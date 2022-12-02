1500fdd6185ebe3df563d39f659f276ee0a22028
ivanarena <ivan.x28x@gmail.com>
2022-12-02 11:14:29 +0100

    made test pages

--- php/pageBuilder.php
+++ php/pageBuilder.php
@@ -5,6 +5,24 @@ class PageBuilder {
     private $pageName;
     private $path;
     private $content;
+
+    public function __construct(string $_pageName, string $_path) {
+        $this->pageName = $_pageName;
+        $this->path = $_path;
+        $this->content = file_get_contents(__DIR__ . "/pages/" . $this->_pageName . ".html");
+    }
+
+    public function setHead();
+    
+    public function setHeader();
+    
+    public function setBreadcrumb();
+    
+    public function setFooter();
+    
+    public function buildPage() {
+        return $content;
+    }
 }
 
 ?>
