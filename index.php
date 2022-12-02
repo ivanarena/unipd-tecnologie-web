f53ad56dce7d1f71f68602072cfba78f4c1e1851
ivanarena <ivan.x28x@gmail.com>
2022-12-02 12:23:28 +0100

    pageBuilder test funziona

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
+    require_once(__DIR__.'/php/pageBuilder.php');
 
-    $page = file_get_contents(__DIR__."/php/pages/index.html");
+    $builder = new PageBuilder('index', '.');
+    $builder->setHead(file_get_contents(__DIR__."/php/pages/components/head.html"));
+    $page = $builder->buildPage();
     echo $page;
-
 ?>
--- php/pageBuilder.php
+++ php/pageBuilder.php
@@ -9,19 +9,24 @@ class PageBuilder {
     public function __construct(string $_pageName, string $_path) {
         $this->pageName = $_pageName;
         $this->path = $_path;
-        $this->content = file_get_contents(__DIR__ . "/pages/" . $this->_pageName . ".html");
+        // $this->content = file_get_contents(__DIR__ . "/pages/" . $this->_pageName . ".html");
+        $this->content = file_get_contents(__DIR__ . "/pages/index.html");
+        // $this->content = file_get_contents(__DIR__ ."/index.html");
     }
 
-    public function setHead();
+    public function setHead(string $head) {
+        $head = str_replace('titleToReplace', 'Home - San Junipero', $head); // TODO: cambiare titleToReplace con tutta la stringa da head in poi
+        $this->content = str_replace('toReplace', $head, $this->content);
+    }
     
-    public function setHeader();
+    public function setHeader() {}
     
-    public function setBreadcrumb();
+    public function setBreadcrumb() {}  
     
-    public function setFooter();
+    public function setFooter() {}
     
     public function buildPage() {
-        return $content;
+        return $this->content;
     }
 }
 
