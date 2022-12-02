533cadcdccd9cd90c82a8af44303cf80268e4646
ivanarena <ivan.x28x@gmail.com>
2022-12-02 14:34:08 +0100

    continuo a costruire pageBuilder

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
     $builder = new PageBuilder('index', '.');
     $builder->setHead(file_get_contents(__DIR__."/php/pages/components/head.html"));
+    $builder->setHeader(file_get_contents(__DIR__."/php/pages/components/header.html"));
+    $builder->setFooter(file_get_contents(__DIR__."/php/pages/components/footer.html"));
     $page = $builder->buildPage();
     echo $page;
 ?>
--- php/pageBuilder.php
+++ php/pageBuilder.php
@@ -9,21 +9,24 @@ class PageBuilder {
     public function __construct(string $_pageName, string $_path) {
         $this->pageName = $_pageName;
         $this->path = $_path;
-        // $this->content = file_get_contents(__DIR__ . "/pages/" . $this->_pageName . ".html");
         $this->content = file_get_contents(__DIR__ . "/pages/index.html");
-        // $this->content = file_get_contents(__DIR__ ."/index.html");
     }
 
     public function setHead(string $head) {
-        $head = str_replace('titleToReplace', 'Home - San Junipero', $head); // TODO: cambiare titleToReplace con tutta la stringa da head in poi
-        $this->content = str_replace('toReplace', $head, $this->content);
+        $head = str_replace('titleToReplace', 'Home - San Junipero', $head);
+        $this->content = str_replace('<headPlaceholder />', $head, $this->content);
     }
     
-    public function setHeader() {}
+    public function setHeader(string $header) {
+        // $this->content = str_replace('', $header, $this->content);
+        $this->content = str_replace('<headerPlaceholder />', $header, $this->content);
+    }
     
     public function setBreadcrumb() {}  
     
-    public function setFooter() {}
+    public function setFooter(string $footer) {
+        $this->content = str_replace('<footerPlaceholder />', $footer, $this->content);
+    }
     
     public function buildPage() {
         return $this->content;
