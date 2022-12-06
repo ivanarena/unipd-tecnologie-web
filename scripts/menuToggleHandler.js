a1ecd63c473f11e17017ec40f1e52c44a61de37e
ivanarena <ivan.x28x@gmail.com>
2022-12-06 22:12:14 +0100

    fatto menu responsive

--- scripts/menuToggleHandler.js
+++ scripts/menuToggleHandler.js
@@ -1,12 +1,30 @@
 let menuToggle = document.getElementById('mobile-menu');
 let pagesMenu = document.getElementById('menu-nav');
 let userMenu = document.getElementById('user-menu-nav');
+let breadcrumb = document.getElementById('breadcrumb-nav');
+let sanJunipero = document.getElementById('san-junipero');
+
+const mobile = window.matchMedia("(max-width: 728px)");
+
+let toHide = [sanJunipero, pagesMenu, userMenu];
+
+if (mobile.matches) {
+    hideElements();
+}
+
 
 function toggleMenu() {
     menuToggle.classList.toggle('is-active');
-    console.log(menuToggle);
-    console.log(pagesMenu);
-    console.log(userMenu);
+    pagesMenu.classList.toggle('hide');
+    pagesMenu.classList.toggle('menu-is-mobile');
+    userMenu.classList.toggle('hide');
+    userMenu.classList.toggle('user-menu-is-mobile');
+    breadcrumb.classList.toggle('hide');
 }
 
-addEventListener('click', toggleMenu);
+
+function hideElements() {
+    toHide.forEach(x => x.classList.add('hide'))
+}
+
+menuToggle.addEventListener('click', toggleMenu);
