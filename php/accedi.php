ca8fcb082d4a53a04fecc9d82a41de154be78e97
ivanarena <ivan.x28x@gmail.com>
2022-12-07 11:49:38 +0100

    cominciato accedi e registrati

---
+++ php/accedi.php
@@ -0,0 +1,12 @@
+<?php 
+    require_once(__DIR__.'/pageBuilder.php');
+
+    // Page information 
+    $title = 'Accedi';
+    $desc = 'blabalb';
+
+    $builder = new PageBuilder('/pages/accedi.html', $title, $desc);
+
+    $page = $builder->buildPage();
+    echo $page;
+?>
---
+++ php/registrati.php
@@ -0,0 +1,12 @@
+<?php 
+    require_once(__DIR__.'/pageBuilder.php');
+
+    // Page information 
+    $title = 'Registrati';
+    $desc = 'blabalb';
+
+    $builder = new PageBuilder('/pages/registrati.html', $title, $desc);
+
+    $page = $builder->buildPage();
+    echo $page;
+?>
