5e7961b027467722eda3200ad8596b0e7ad1e751
giuliadentone <giuliaadentone@gmail.com>
2022-12-12 14:45:21 +0100

    in corso il chech dell'accesso

--- scripts/loginChecker.js
+++ scripts/loginChecker.js
@@ -1,21 +1,50 @@
 const loginButton = document.getElementById('accedi');
+let userName = document.getElementById('username').value; 
+let password = document.getElementById("password").value;  
 
-function checkEmail() {
-
-    console.log('email ok');
+function checkUsername() {
+    userName = document.getElementById('username').value; 
+    if(userName == "") {   
+        alert("Inserisci il Nome Utente");  
+        return false;  
+    }
+    else {
+        return true;
+    }  
+    console.log('username ok');
 }
 
-function checkPassword() {
-
+function checkPassword() { 
+    password = document.getElementById("password").value; 
+    if(password == "") {   
+        alert("Inserisci la password");  
+        return false;  
+    }  
+   /* if(password.length < 8) {  
+        alert("La password deve essere lunga almeno 8 caratteri");
+        return false;  
+    }
+    if(password.length > 15) {  
+        alert("La password non può essere più lunga di 15 caratteri");
+        return false;  
+    } */
+    else {  
+        alert("La password è corretta");  
+    }  
     console.log('password ok');
 }
 
 
 
 function checkForm() {
-    if (checkEmail() && checkPassword()) {
+    if (checkUsername() && checkPassword()) {
         console.log('ok');
-
+    }
+    else if (!checkUsername() && checkPassword()) {
+        alert('Inserire un Nome Utente Valido');
+    }
+    else {
+        alert('Inserire una Password')
     }
     console.log('bottone premuto');
 }
