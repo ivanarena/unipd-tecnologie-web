007590eab7c39f7a14e216e8b90a29f3c3db8832
ivanarena <ivan.x28x@gmail.com>
2022-12-14 14:25:29 +0100

    quasi finita dark mode

--- scripts/darkModeHandler.js
+++ scripts/darkModeHandler.js
@@ -1,2 +1,26 @@
-console.log('dark mode');
-const darkMode = document.getElementById('dark-mode-switch');
+const darkModeSwitch = document.getElementById('dark-mode-switch');
+const darkModeLink = document.createElement("link");
+darkModeLink.type = "text/css";
+darkModeLink.rel = "stylesheet";
+darkModeLink.href = '/styles/dark-mode.css';
+
+// cambiare con dark -- sto usando questo per testare
+let prefersDark = window.matchMedia("(prefers-color-scheme: light)").matches;
+
+if (prefersDark && !darkModeSwitch.checked) {
+    darkModeSwitch.checked = true;
+    toggleDarkMode();
+}
+
+function toggleDarkMode() {
+    let head = document.getElementsByTagName("head")[0];
+    if (darkModeSwitch.checked) {
+        head.appendChild(darkModeLink);
+    } else {
+
+        head.removeChild(darkModeLink);
+    }
+}
+
+
+darkModeSwitch.addEventListener('change', toggleDarkMode);
