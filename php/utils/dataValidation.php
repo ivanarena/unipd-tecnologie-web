585533541083133d65abdd7f6c5606a8d61ee74e
Alyoninthecity <53126736+Alyoninthecity@users.noreply.github.com>
2022-12-14 18:30:25 +0100

    Modificati gli errori

--- php/utils/dataValidation.php
+++ php/utils/dataValidation.php
@@ -1,19 +1,3 @@
 <?php require('session.php'); ?>
 <?php 
-function checkUsername($username){
-    if (!empty($_POST)) {
-        $valid = true;
-        if (array_key_exists("bUsername", $_REQUEST) && !empty($_REQUEST["bUsername"]) && strlen($_REQUEST["bUsername"]) <= 100) {
-            $username = $_POST['bUsername'];
-        } else {
-            $usernameError = 'Inserire un username';
-            $valid = false;
-        };
-        if (array_key_exists("bUsername", $_REQUEST) && !empty($_REQUEST["bPassword"]) && strlen($_REQUEST["bPassword"]) <= 100) {
-            $password = $_POST['bPassword'];
-        } else {
-            $passwordError = 'Inserire una password';
-            $valid = false;
-        }
-    }
-}
+?>
