87dee24f02a7d1e24b6939b8c2fdf8a4bf2633c9
ivanarena <ivan.x28x@gmail.com>
2022-12-18 12:55:56 +0100

    ancora test su doregistrati

--- php/registrati.php
+++ php/registrati.php
@@ -8,6 +8,15 @@
 
     $builder = new PageBuilder($fileName, $desc);
 
+    if(!empty($_GET)){
+        if(array_key_exists("errUser", $_REQUEST) && !empty($_REQUEST["errUser"])){
+            $builder->setError("Il nome utente non &egrave;	corretto.",$_GET["errUser"]);  // TODO: Stilizzare meglio
+        }
+        if(array_key_exists("errGen", $_REQUEST) && !empty($_REQUEST["errGen"])){
+            $builder->setError("Campi non validi!!!",$_GET["errGen"]); // TODO: Stilizzare meglio
+        }
+    }
+
     $page = $builder->buildPage();
     echo $page;
 ?>
