8412f0fe56ffbbe3f9ebec2b3517fbdf80979fcc
ivanarena <ivan.x28x@gmail.com>
2022-12-19 13:00:04 +0100

    iniziata pagina acquista

---
+++ php/acquista.php
@@ -0,0 +1,13 @@
+<?php require_once('session.php'); ?>
+<?php 
+    require_once(__DIR__.'/pageBuilder.php');
+    
+    // Page information 
+    $fileName = 'acquista';
+    $desc = 'blabalb';
+
+    $builder = new PageBuilder($fileName, $desc);
+
+    $page = $builder->buildPage();
+    echo $page;
+?>
