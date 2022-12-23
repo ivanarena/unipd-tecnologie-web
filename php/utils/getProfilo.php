afc25ed30c0f2ea342a1316e1a203f005c7d1747
ivanarena <ivan.x28x@gmail.com>
2022-12-23 13:14:29 +0100

    profilo e prenotazione quasi finiti

---
+++ php/utils/getProfilo.php
@@ -0,0 +1,21 @@
+<?php 
+require_once("database.php");
+
+function getAnagrafica() {
+    $result = "";
+    try {
+        $pdo = database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $sql = "SELECT * FROM EVENTO;";
+        database::disconnect();
+    } catch (PDOException $e) {
+                echo 'Errore PDO e connessione DB: <br />';
+                echo 'SQLQuery: ', $sql;
+                echo 'Errore: ' . $e->getMessage();
+        }
+    return $result;
+}
+
+
+?>
