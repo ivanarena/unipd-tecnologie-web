8cb3021953b87b21155bef2354cce3791a023444
ivanarena <ivan.x28x@gmail.com>
2022-12-23 12:09:49 +0100

    reso locali dinamica

--- php/locali.php
+++ php/locali.php
@@ -1,12 +1,15 @@
 <?php require_once('session.php'); ?>
 <?php 
     require_once(__DIR__.'/pageBuilder.php');
+    require_once(__DIR__.'/utils/getLocali.php');
 
     // Page information 
     $fileName = 'locali';
     $desc = 'blabalb';
 
     $builder = new PageBuilder($fileName, $desc);
+
+    $builder->setContent("<localiPlaceholder />", getLocali());
 
     $page = $builder->buildPage();
     echo $page;
---
+++ php/utils/getLocali.php
@@ -0,0 +1,37 @@
+<?php 
+require_once("database.php");
+
+function getLocali(){
+    $result = "";
+    try{
+        $pdo = database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $sql = "SELECT * FROM LOCALE;";
+        foreach ($pdo->query($sql)->fetchAll() as $locale) {
+            $result.= strval('<div class="h-card card-shadow">
+            <img class="place-img" src="/images/locali/fluidocafe.jpg" alt="immagine $" />
+            <div class="place-info-container">
+
+                <div class="place-text">
+                    <h1 class="place-name">' . $locale["NomeLocale"] . '</h1>
+                    <h2 class="place-street">' . $locale["Indirizzo"] . '</h2>
+                    <p class="place-desc">' . $locale["Descrizione"] . '</p>
+                </div>
+                <div class="btn-container">
+                    <a class="btn primary-btn place-events-btn" href="">Controlla gli eventi</a>
+                </div>
+            </div>
+        </div>');
+    }
+    database::disconnect();
+    } catch (PDOException $e) {
+    echo 'Errore PDO e connessione DB: <br />';
+    echo 'SQLQuery: ', $sql;
+    echo 'Errore: ' . $e->getMessage();
+    }
+    return $result;
+}
+
+
+?>
