cad707d5e9dc449e9163414c33a7fe18096071aa
ivanarena <ivan.x28x@gmail.com>
2022-12-29 12:35:01 +0100

    eventi dinamica

--- php/utils/getEventi.php
+++ php/utils/getEventi.php
@@ -7,20 +7,24 @@ function getEventi() {
         $pdo = database::connect();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $sql = "SELECT * FROM EVENTO;";
+        if (empty($_GET)) {
+            $sql = "SELECT * FROM EVENTO AS E, LOCALE AS L WHERE L.IdLocale=E.IdLocale;";
+        } else {
+            $sql = 'SELECT * FROM EVENTO AS E, LOCALE AS L WHERE L.IdLocale=E.IdLocale AND E.IdLocale="'. $_GET["IdLocale"] .'";';
+        }
         foreach ($pdo->query($sql)->fetchAll() as $evento) {
-            $result.= strval('<div class="h-card card-shadow events-container">
+            $result .= strval('<div class="h-card card-shadow events-container">
             <div class="event-card">
                 <div class="event-text">
                     <div class="event-heading">
                         <h1 class="event-title">' . $evento["NomeEvento"] . '</h1>
-                        <h2 class="event-place">???</h2>
+                        <h2 class="event-place">'. $evento["NomeLocale"] .'</h2>
                         <h3 class="event-timestamp"><time datetime="2023-01-15">15/01/2023</time>, dalle 17 alle 19</h3>
                     </div>
                     <p class="event-desc">' . $evento["Descrizione"] . '</p>
                 </div>
                 <div class="booking-container flex-col-center">
-                    <button class="btn primary-btn booking-btn">Prenota</button>
+                    <a role="button" class="btn primary-btn booking-btn">Prenota</a>
                     <span class="spots-available">???</span>
                 </div>
             </div>
