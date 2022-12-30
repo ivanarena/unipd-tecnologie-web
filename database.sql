70f69b1aa7ccb1c270802c00519618171b254a89
giuliadentone <giuliaadentone@gmail.com>
2022-12-30 14:50:40 +0100

    sistemato database

--- database.sql
+++ database.sql
@@ -39,12 +39,11 @@ CREATE TABLE CARTA(
     PRIMARY KEY(NumeroCarta)
     );
 
-CREATE TABLE ABBONAMENTO( /**togliere durata?*/
+CREATE TABLE ABBONAMENTO(
     IdAbbonamento CHAR(1) NOT NULL AUTO_INCREMENT,
     TitoloAbb VARCHAR(100) NOT NULL, 
     Descrizione VARCHAR(1024), 
     Prezzo DECIMAL(10,2) NOT NULL, 
-    DurataAbb INT NOT NULL,
     EventiSettimanali INT NOT NULL,
     MetaCoin DECIMAL(10,2) NOT NULL default 0.00,
     PRIMARY KEY(IdAbbonamento)
@@ -64,7 +63,6 @@ CREATE TABLE EVENTO(
     IdEvento INT NOT NULL AUTO_INCREMENT, 
     NomeEvento VARCHAR(100) NOT NULL, 
     Descrizione VARCHAR(1024) NOT NULL, 
-    LinkImg VARCHAR(512), 
     IdLocale INT NOT NULL, 
     DataOraInizio DATETIME NOT NULL, 
     DataOraFine DATETIME NOT NULL,
@@ -120,10 +118,10 @@ INSERT INTO CARTA VALUES("583584310887", "Maestro", "026", "2027-01-01", "Simone
 INSERT INTO CARTA VALUES("783584310000", "Maestro", "021", "2023-01-01", "Luca Galeazzo");
 INSERT INTO CARTA VALUES("111562587000", "Maestro", "057", "2025-05-12", "Mario Rossi");
 
-/**ABBONAMENTI sistemare durata**/
-INSERT INTO ABBONAMENTO VALUES("1", "Basic", "NULL", "100", "1", "1", "1000");
-INSERT INTO ABBONAMENTO VALUES("2", "Pro", "NULL", "220", "1", "5", "3000");
-INSERT INTO ABBONAMENTO VALUES("3", "Superior", "NULL", "365", "1", "10", "7000");
+/**ABBONAMENTI**/
+INSERT INTO ABBONAMENTO VALUES("1", "Basic", "NULL", "100", "1", "1000");
+INSERT INTO ABBONAMENTO VALUES("2", "Pro", "NULL", "220", "5", "3000");
+INSERT INTO ABBONAMENTO VALUES("3", "Superior", "NULL", "365", "10", "7000");
 
 /**LOCALI **/
 INSERT INTO LOCALE VALUES("1", "Fluido Caf&egrave;", "Via Pirandello, 65", "Caffetteria con un'ampia selezione di libri da cui attingere per immergersi in una rilassante lettura mentre si assaporano caffè e dolci fatti in casa.", "50", "/images/locali/fluido_cafe.jpg");
@@ -133,13 +131,85 @@ INSERT INTO LOCALE VALUES("4", "Bagni Venere", "Via Fratelli Cervi, 73", 'Piscin
 INSERT INTO LOCALE VALUES("5", "Sala Conferenze Dante Alighieri", "Via Dante Alighieri, 1", 'Sala conferenze che ospita presentazioni di libri, comizi, interviste e addirittura numerosi <span lang="en">TedX</span>. Posto perfetto per ampliare la propria cultura e i propri orizzonti.', "300", "/images/locali/sala_conferenze_dante_a.jpg");
 
 /**EVENTI **/ 
--- ! MANCA LINK IMG
-INSERT INTO EVENTO VALUES("1", "Salone del libro", "NULL", "", "1", "2023-01-15 17:00:00", "2023-01-15 19:00:00");
-INSERT INTO EVENTO VALUES("2", "Vida Loca", "NULL", "", "2", "2023-01-02 22:00:00", "2023-02-03 06:00:00");
-INSERT INTO EVENTO VALUES("3", "Spirit techno", "NULL", "", "2", "2022-12-26 22:00:00", "2022-12-27 06:00:00");
-INSERT INTO EVENTO VALUES("4", "Ride", "NULL", "", "3", "2022-12-29 09:00:00", "2022-12-29 20:00:00");
-INSERT INTO EVENTO VALUES("5", "Day Spa", "NULL", "", "4", "2022-12-30 08:00:00", "2022-12-30 23:00:00");
-INSERT INTO EVENTO VALUES("6", "Lotta alla mafia con Saviano", "NULL", "", "5", "2023-01-05 17:00:00", "2023-01-05 20:30:00");
+INSERT INTO EVENTO VALUES("1", "Salone del libro", "Il salone del libro è un evento frequentemente tenuto al Fluido Cafè e che
+                            ha
+                            come obiettivo quello di promuovere
+                            la lettura e il libro come mezzo di conoscenza e intrattenimento. Durante il salone del
+                            libro,
+                            vengono organizzati incontri con
+                            autori, presentazioni di libri, tavole rotonde e altre attività culturali. Il salone del
+                            libro è
+                            aperto a tutti e rappresenta
+                            un'occasione unica per conoscere le novità editoriali, scambiare opinioni con gli autori e
+                            gli
+                            esperti del settore e partecipare
+                            a interessanti dibattiti su temi culturali e sociali. Inoltre, il salone del libro offre
+                            l'opportunità di acquistare libri a
+                            prezzi scontati e di partecipare a <span lang="en">workshop</span> e laboratori dedicati
+                            alla
+                            scrittura e all'editoria. Insomma, il salone del libro
+                            è un appuntamento imperdibile per gli appassionati di lettura e per chi vuole essere sempre
+                            aggiornato sull'evoluzione del mondo
+                            dell'editoria.", "1", "2023-01-15 17:00:00", "2023-01-15 19:00:00");
+INSERT INTO EVENTO VALUES("2", "Vida Loca", "Vieni a scatenarti al ritmo del raggaeton più travolgente della stagione!
+                            La
+                            nostra discoteca
+                            si trasformerà in un'arena dove potrai ballare e cantare insieme ai tuoi amici alle <span
+                                lang="en">hit</span> del momento e alle canzoni
+                            storiche del genere. Le luci e le decorazioni a tema ti faranno immergere completamente
+                            nell'atmosfera latina,
+                            mentre il <span lang="en"><abbr title="disc jockey">DJ</abbr></span> mixerà le canzoni per
+                            creare una serata indimenticabile. Ma non è tutto: durante la serata ci saranno
+                            anche animazioni e performance di ballo per rendere la festa ancora più coinvolgente. Non
+                            perdere l'occasione
+                            di vivere una notte indimenticabile all'insegna del divertimento e della musica latina!", "2", "2023-01-02 22:00:00", "2023-02-03 06:00:00");
+INSERT INTO EVENTO VALUES("3", "Spirit techno", "Venite a vivere la notte più emozionante della vostra vita al nostro
+                            evento
+                            <span lang="en">techno</span> in discoteca!
+                            Preparatevi a sentire il battito delle vostre pulsazioni al ritmo delle migliori tracce
+                            <span lang="en">techno</span>, mentre vi lasciate
+                            trasportare dalle luci stroboscopiche e dall'atmosfera elettrizzante della nostra pista da
+                            ballo. Con i nostri <span lang="en"><abbr title="disc jockey">DJ</abbr></span>
+                            di fama internazionale ai piatti, questa è una serata che non dimenticherete facilmente. Non
+                            perdete l'opportunità
+                            di unirvi a noi e scatenarvi al ritmo della <span lang="en">techno</span> più pura!", "2", "2022-12-26 22:00:00", "2022-12-27 06:00:00");
+INSERT INTO EVENTO VALUES("4", "Ride", "Vieni a vivere l'adrenalina delle corse sui <span lang="en">kart</span> in
+                            una
+                            <span lang="en">location</span> mozzafiato! In <span lang="en">indoor</span> e <span
+                                lang="en">oudoor</span>,
+                            potrai sfrecciare su piste sinuose e sfidare gli avversari in emozionanti gare. Con la
+                            nostra
+                            struttura
+                            all'avanguardia e il personale altamente qualificato, ti garantiamo sicurezza e divertimento
+                            assoluti.
+                            Non perdere l'occasione di vivere una giornata indimenticabile alla guida dei nostri <span
+                                lang="en">kart</span> performanti.
+                            Prenota subito la tua esperienza di corse mozzafiato!", "3", "2022-12-29 09:00:00", "2022-12-29 20:00:00");
+INSERT INTO EVENTO VALUES("5", "Day Spa", "Una giornata in spa è il modo perfetto per rilassarsi e coccolarsi.
+                            Immagina
+                            di passare
+                            la giornata in un ambiente tranquillo e rilassante, circondato da bellezze naturali e con
+                            una
+                            vista mozzafiato.
+                            Potrai godere delle piscine termali calde e rilassanti, che ti aiuteranno a sciogliere ogni
+                            tensione muscolare.
+                            Inoltre, potrai scegliere tra una varietà di massaggi, che ti aiuteranno a rilassare il
+                            corpo e
+                            la mente.
+                            E per rendere la tua giornata ancora più speciale, c'è anche un open bar con bevande
+                            rinfrescanti e <span lang="en">snack</span>
+                            leggeri, perfetti per rilassarsi al sole. Tutto questo ti aspetta in una giornata in spa,
+                            dove
+                            potrai godere
+                            di momenti di benessere e <span lang="en">relax</span> assoluti.", "4", "2022-12-30 08:00:00", "2022-12-30 23:00:00");
+INSERT INTO EVENTO VALUES("6", "Lotta alla mafia con Saviano", "Vi invitiamo a unirvi a noi per ascoltare Roberto Saviano, autore e
+                            giornalista italiano che
+                            ha dedicato la sua carriera a denunciare il potere e la violenza della camorra e della
+                            mafia.
+                            Durante il suo
+                            comizio, Saviano condividerà la sua profonda conoscenza della storia e delle dinamiche della
+                            camorra,
+                            e racconterà le sue personali esperienze nella lotta contro queste organizzazioni criminali.", "5", "2023-01-05 17:00:00", "2023-01-05 20:30:00");
 
 /**PORTAFOGLI**/
 INSERT INTO PORTAFOGLIO VALUES("Alyon", "3704061262648614");
