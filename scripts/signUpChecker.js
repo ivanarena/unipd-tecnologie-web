5de1de616c46296cdb6753fb3a10b58c92364f86
giuliadentone <giuliaadentone@gmail.com>
2023-01-05 17:36:48 +0100

    sistemato i messaggi di errore di registrati

--- scripts/signUpChecker.js
+++ scripts/signUpChecker.js
@@ -4,42 +4,42 @@ const passwordInput = document.getElementById("password");
 const passwordRepeatInput = document.getElementById("password-repeat");
 
 let userName = document.getElementById('username');
-let password = document.getElementById("password").value;
-let sname = document.getElementById("nome").value;
-let surname = document.getElementById("cognome").value;
-let dateOfBirth = document.getElementById("data").value;
-let password2 = document.getElementById("password-repeat").value;
-let email = document.getElementById("email").value;
+let password = document.getElementById("password");
+let sname = document.getElementById("nome");
+let surname = document.getElementById("cognome");
+let dateOfBirth = document.getElementById("data");
+let password2 = document.getElementById("password-repeat");
+let email = document.getElementById("email");
 
 function checkName() {
-    sname = document.getElementById("nome").value;
+    let snamev = sname.value;
     if (sname == "") {
-        alert("Inserisci il tuo nome");
+        document.getElementById("nameErr").innerHTML = "Inserisci il tuo nome";
         return false;
     }
     else if (sname.length > 50) {
-        alert("Inserisci un nome valido");
+        document.getElementById("nameErr").innerHTML = "Inserisci un nome valido";
         return false;
     }
     else {
+        document.getElementById("nameErr").innerHTML = ""
         return true;
-        console.log('nome ok');
     }
 }
 
 function checkSurname() {
-    surname = document.getElementById("cognome").value;
+    let surnamev = surname.value;
     if (surname == "") {
-        alert("Inserisci il tuo cognome");
+        document.getElementById("surnameErr").innerHTML = "Inserisci il tuo cognome";
         return false;
     }
     else if (surname.length > 50) {
-        alert("Inserisci un cognome valido");
+        document.getElementById("surnameErr").innerHTML = "Inserisci un cognome valido";
         return false;
     }
     else {
-        return true; s
-        console.log('cognome ok');
+        document.getElementById("surnameErr").innerHTML = ""
+        return true; 
     }
 }
 
@@ -50,29 +50,29 @@ function checkUsername() {
         return false;
     }
     else if (usernamev.length > 20) {
-        alert("Il nome utente deve avere massimo 20 caratteri");
+        document.getElementById("usernameErr").innerHTML = "Il nome utente deve avere massimo 20 caratteri";
         return false;
     }
     else {
         document.getElementById("usernameErr").innerHTML = "";
         return true;
-        console.log('username ok');
     }
 }
 
 function checkEmail() {
-    email = document.getElementById("email").value;
+    let emailv = email.value;
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        document.getElementById("emailErr").innerHTML = ""
         return true;
     }
     else {
-        alert("Indirizzo email non valido, riprova");
+        document.getElementById("emailErr").innerHTML = "Indirizzo email non valido, riprova";
         return false;
     }
 }
 
 function checkDataNascita() {
-    dateOfBirth = document.getElementById("data").value;
+    let dateOfBirthv = dateOfBirth.value;
     var today = new Date();
     var birthDate = new Date(dateOfBirth);
     var age = today.getFullYear() - birthDate.getFullYear();
@@ -80,56 +80,54 @@ function checkDataNascita() {
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
         age--;
     }
-    console.log(age);
     if (age > 17) {
+        document.getElementById("dateErr").innerHTML = "";
         return true;
-        console.log('data ok');
     }
     else {
-        alert('Ci dispiace, questo servizio è riservato ai maggiorenni!')
+        document.getElementById("dateErr").innerHTML = "Ci dispiace, questo servizio è riservato ai maggiorenni!";
         return false;
     }
 }
 
 function checkPassword() {
-    password = document.getElementById("password").value;
+    let passwordv = password.value;
     if (password == "") {
-        alert("Inserisci la password");
+        document.getElementById("passErr").innerHTML = "Inserisci la password";
         return false;
     }
     else if (password.length < 8) {
-        alert("La password deve essere lunga almeno 8 caratteri");
+        document.getElementById("passErr").innerHTML = "La password deve essere lunga almeno 8 caratteri";
         return false;
     }
     else if (password.length > 50) {
-        alert("La password non può essere più lunga di 15 caratteri");
+        document.getElementById("passErr").innerHTML = "La password non può essere più lunga di 15 caratteri";
         return false;
     }
     else {
-        alert("La password è corretta");
+        document.getElementById("passErr").innerHTML = "La password è corretta";
         return true;
-        console.log('password ok');
     }
 }
 
 function checkEqualPassword() {
-    password2 = document.getElementById("password-repeat").value;
+    let password2v = passoword2.value;
     if (password == password2) {
+        document.getElementById("pass2Err").innerHTML = "";
         return true;
-        console.log('password2 ok');
     }
     else {
+        document.getElementById("pass2Err").innerHTML = "La password è corretta";
         alert("La password inserita non corrisponde alla precendente. Riprova");
         return false;
     }
 }
 
-function checkForm() {
+/*function checkForm() {
     if (checkUsername() && checkPassword() && checkName() && checkSurname() && checkDataNascita() && checkEqualPassword() && checkEmail()) {
-        console.log('ok');
+
     }
-    console.log('bottone premuto');
-}
+}*/
 
 function showPassword() {
     console.log(passwordInput)
@@ -143,4 +141,11 @@ function showPassword() {
 }
 
 showPasswordButton.addEventListener('change', showPassword);
+//signUpButton.addEventListener('onclick', checkForm);
 userName.addEventListener('focusout', checkUsername);
+sname.addEventListener('focusout', checkName);
+surname.addEventListener('focusout', checkSurname);
+email.addEventListener('focusout', checkEmail);
+dateOfBirth.addEventListener('focusout', checkDataNascita);
+password.addEventListener('focusout', checkPassword);
+password2.addEventListener('focusout', checkEqualPassword);
