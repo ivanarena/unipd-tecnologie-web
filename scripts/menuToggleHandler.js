c593bef83a5d539e9a7d02be96145c90d67bb209
ivanarena <ivan.x28x@gmail.com>
2023-01-18 11:02:45 +0100

    fixato menu

--- scripts/menuToggleHandler.js
+++ scripts/menuToggleHandler.js
@@ -6,27 +6,45 @@ const sanJunipero = document.getElementById('san-junipero');
 const sanJuniperoLogo = document.getElementById('san-junipero-logo');
 const darkModeToggle = document.getElementById('dark-mode-toggle')
 
-const mobile = window.matchMedia("(max-width: 1400px)");
+const mobile = window.matchMedia("(max-width: 938px)");
+const smallDesktop = window.matchMedia("(max-width: 1400px)");
 // const desktop = window.matchMedia("(min-width: 729px)");
 
-const toHide = [sanJunipero, pagesMenu, userMenu];
+const toHide = [pagesMenu, userMenu];
 const toFix = [sanJuniperoLogo, menuToggle];
 
+let smallDesktopToggled = false;
 let mobileToggled = false;
-if (mobile.matches) {
+if (smallDesktop.matches) {
+    smallDesktopToggled = true;
+    sanJunipero.classList.toggle('hide');
+}
+else if (mobile.matches) {
     mobileToggled = true;
     menuToggle.classList.toggle('hide');
     toggleHideElements();
 }
 
 function mobileViewToggle() {
-    if (mobile.matches && !mobileToggled) {
+    if (smallDesktop.matches && !smallDesktopToggled) {
+        smallDesktopToggled = true;
+        sanJunipero.classList.toggle('hide');
+    } else if (!smallDesktop.matches && smallDesktopToggled) {
+        smallDesktopToggled = false;
+        sanJunipero.classList.toggle('hide');
+    } else if (mobile.matches && !mobileToggled) {
         mobileToggled = true;
         menuToggle.classList.toggle('hide');
         toggleHideElements();
     } else if (!mobile.matches && mobileToggled) {
         mobileToggled = false;
         menuToggle.classList.toggle('hide');
+        if (smallDesktopToggled) {
+            if (!smallDesktop.matches) {
+                smallDesktopToggled = false;
+                sanJunipero.classList.toggle('hide');
+            }
+        }
         toggleHideElements();
     }
 }
