d03cd1f6262c63f87b9549c86e2a59ab0ff732aa
Fbombi <filippo.bomben@studenti.unipd.it>
2023-01-19 15:52:36 +0100

    Update creaEventiChecker.js

--- scripts/creaEventiChecker.js
+++ scripts/creaEventiChecker.js
@@ -1,17 +1,21 @@
-let date1 = document.getElementById("data-inizio-evento").value;
-let date2 = document.getElementById("data-fine-evento").value;
+let date1 = document.getElementById("data-inizio-evento");
+let date2 = document.getElementById("data-fine-evento");
 
-let d1 = Date.parse(date1);
-let d2 = Date.parse(date2);
 
 function checkDate() {
-    if(d1 > d2){
-        document.getElementById("dateErr").classList.add('hide');
-        return true;
+    let d1 = date1.value;
+    let d2 = date2.value;
+    var inizioEv = new Date(d1);
+    var fineEv = new Date(d2);
+    if ((inizioEv.getFullYear() > fineEv.getFullYear()) && (inizioEv.getMonth() > fineEv.getMonth()) && (inizioEv.getDay() > fineEv.getDay())){
+        document.getElementById("dateErr").classList.add('hide');;
+        return false;
     }
     else {
         document.getElementById("dateErr").classList.remove('hide');
-        return false;
+        return true;
     }
 }
-;
+
+date1.addEventListener('focusout', checkDate);
+date2.addEventListener('focusout', checkDate);
