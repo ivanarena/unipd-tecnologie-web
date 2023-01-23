85ef701f1f4ea75691f0acfa0db42051c8c87502
Alyoninthecity <53126736+Alyoninthecity@users.noreply.github.com>
2023-01-23 16:26:10 +0100

    Creazione Evento solo admin

--- php/utils/creaEvento.php
+++ php/utils/creaEvento.php
@@ -1,16 +1,20 @@
 <?php require_once('../session.php'); ?>
 <?php 
-    require_once('../pageBuilder.php');
-    require_once('getLocali.php');
+if (isset($_SESSION["Username"])&& isset($_SESSION["admin"])){
+    if($_SESSION["admin"]==1){
+        require_once('../pageBuilder.php');
+        require_once('getLocali.php');
 
-    // Page information 
-    $fileName = 'crea-evento';
-    $desc = 'Crea un nuovo evento.';
+        // Page information 
+        $fileName = 'crea-evento';
+        $desc = 'Crea un nuovo evento.';
 
-    $builder = new PageBuilder($fileName, $desc);
+        $builder = new PageBuilder($fileName, $desc);
 
-    $builder->setContent("<getLocali />", getLocaliAsOptions());
+        $builder->setContent("<getLocali />", getLocaliAsOptions());
 
-    $page = $builder->buildPage();
-    echo $page;
+        $page = $builder->buildPage();
+        echo $page;
+    }
+}
 ?>
--- php/utils/doCreaEvento.php
+++ php/utils/doCreaEvento.php
@@ -1,24 +1,28 @@
 <?php
 require_once("../session.php");
-    try {
-        include_once('./database.php');
-        $pdo = database::connect();
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $dataOraInizio = new DateTime();
-        $dataOraFine = new DateTime();
-        $dataOraInizio = $dataOraInizio->createFromFormat('Y-m-d*H:i', $_POST['data-inizio-evento']);
-        $dataOraFine = $dataOraInizio->createFromFormat('Y-m-d*H:i', $_POST['data-fine-evento']);
-        $dataOraInizio = $dataOraInizio->format('Y-m-d H:i:s');
-        $dataOraFine = $dataOraFine->format('Y-m-d H:i:s');
-        $sql = 'INSERT INTO EVENTO (NomeEvento, Descrizione, IdLocale, DataOraInizio, DataOraFine) VALUES(?, ?, ?, ?, ?);';
-        $query = $pdo->prepare($sql);
-        $query->execute(array($_POST["nome-evento"], $_POST["descrizione"], $_POST["locale"], $dataOraInizio, $dataOraFine));
-        database::disconnect();
-    } catch (PDOException $e) {
-        echo 'Errore PDO e connessione DB: <br />';
-        echo 'SQLQuery: ', $sql;
-        echo 'Errore: ' . $e->getMessage();
+if (isset($_SESSION["Username"])&& isset($_SESSION["admin"])){
+    if($_SESSION["admin"]==1){
+        try {
+            include_once('./database.php');
+            $pdo = database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dataOraInizio = new DateTime();
+            $dataOraFine = new DateTime();
+            $dataOraInizio = $dataOraInizio->createFromFormat('Y-m-d*H:i', $_POST['data-inizio-evento']);
+            $dataOraFine = $dataOraInizio->createFromFormat('Y-m-d*H:i', $_POST['data-fine-evento']);
+            $dataOraInizio = $dataOraInizio->format('Y-m-d H:i:s');
+            $dataOraFine = $dataOraFine->format('Y-m-d H:i:s');
+            $sql = 'INSERT INTO EVENTO (NomeEvento, Descrizione, IdLocale, DataOraInizio, DataOraFine) VALUES(?, ?, ?, ?, ?);';
+            $query = $pdo->prepare($sql);
+            $query->execute(array($_POST["nome-evento"], $_POST["descrizione"], $_POST["locale"], $dataOraInizio, $dataOraFine));
+            database::disconnect();
+        } catch (PDOException $e) {
+            echo 'Errore PDO e connessione DB: <br />';
+            echo 'SQLQuery: ', $sql;
+            echo 'Errore: ' . $e->getMessage();
+        }
     }
+}
 header('location: ../eventi.php');
 
 ?>
