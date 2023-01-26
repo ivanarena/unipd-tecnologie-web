a636431057d468efd6daa4e368b0ad048bcca4b3
Fbombi <filippo.bomben@studenti.unipd.it>
2023-01-26 15:18:02 +0100

    Per accessibilità

--- scripts/currentPage.js
+++ scripts/currentPage.js
@@ -3,43 +3,48 @@ let current = document.getElementsByClassName("breadcrumb")[0].innerText;
 
 if(current == "Home"){
     var d = document.getElementsByClassName("page")[0];
-    
     d.removeAttribute("href");
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
+
 if(current == "Abbonamenti"){
     var d = document.getElementsByClassName("page")[1];
-    
     d.removeAttribute("href");
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
+
 if(current == "Locali"){
     var d = document.getElementsByClassName("page")[2];
-    
     d.removeAttribute("href");
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
+
 if(current == "Eventi"){
     var d = document.getElementsByClassName("page")[3];
-    
     d.removeAttribute("href");
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
+
 if(current == "Chi siamo"){
     var d = document.getElementsByClassName("page")[4];
-    
     d.removeAttribute("href");
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
+
 if(current == "Contatti"){
     var d = document.getElementsByClassName("page")[5];
-    
     d.removeAttribute("href");
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
