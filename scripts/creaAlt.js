38801febbf047222339b7a2a93dd9dc9045d7f16
Fbombi <filippo.bomben@studenti.unipd.it>
2023-01-26 16:25:17 +0100

    close #27

---
+++ scripts/creaAlt.js
@@ -0,0 +1,14 @@
+var alt0 = document.getElementsByClassName("place-img")[0];
+alt0.setAttribute("alt", "Bar molto luminoso con tavolini in legno e un bancone lungo sempre in legno");
+
+var alt1 = document.getElementsByClassName("place-img")[1];
+alt1.setAttribute("alt", "Discoteca con una grande sala da ballo con palco in posizione centrale e ai lati zone con divanetti. Grandi luci luminose sul soffitto");
+
+var alt2 = document.getElementsByClassName("place-img")[2];
+alt2.setAttribute("alt", "Pista di Kart su più livelli all'interno di una struttura coperta molto luminosa");
+
+var alt3 = document.getElementsByClassName("place-img")[3];
+alt3.setAttribute("alt", "Piscine e terme all'interno di una grotta. L'area è illuminata grazie a dei fori che si trovano nel soffitto dalla luce naturale");
+
+var alt4 = document.getElementsByClassName("place-img")[4];
+alt4.setAttribute("alt", "Sala conferenze ricca di postazioni per sedersi con una forma a semicirconfernza. Le sedie sono poste su più livelli accessibili da una scala che sale al centro della sala");
