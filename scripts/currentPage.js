2d0e8dd4eae96e3d26fc4b91319c43e11e9b1ad3
Fbombi <filippo.bomben@studenti.unipd.it>
2023-01-28 16:02:34 +0100

    aggiunto per non creare errori

--- scripts/currentPage.js
+++ scripts/currentPage.js
@@ -5,6 +5,14 @@ if(current == "Home"){
     var d = document.getElementsByClassName("page")[0];
     d.removeAttribute("href");
 
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
@@ -12,6 +20,14 @@ if(current == "Home"){
 if(current == "Abbonamenti"){
     var d = document.getElementsByClassName("page")[1];
     d.removeAttribute("href");
+
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
 
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
@@ -21,6 +37,14 @@ if(current == "Locali"){
     var d = document.getElementsByClassName("page")[2];
     d.removeAttribute("href");
 
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
@@ -28,6 +52,14 @@ if(current == "Locali"){
 if(current == "Eventi"){
     var d = document.getElementsByClassName("page")[3];
     d.removeAttribute("href");
+
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
 
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
@@ -37,6 +69,14 @@ if(current == "Chi siamo"){
     var d = document.getElementsByClassName("page")[4];
     d.removeAttribute("href");
 
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
@@ -45,56 +85,144 @@ if(current == "Contatti"){
     var d = document.getElementsByClassName("page")[5];
     d.removeAttribute("href");
 
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Registrati"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Profilo"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Pagameto"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Prenotazione"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Crea evento"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Prenotazioni"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Accedi"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Acquista"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Eliminato"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
 
 if(current == "Registrazione"){
+    const link = document.createElement("a");
+    link.setAttribute('class', 'aiuti');
+    link.setAttribute('href', '#contenuto')
+    const node = document.createTextNode("salta la navigazione");
+    link.appendChild(node);
+    const element = document.getElementsByClassName("salto-navigazione")[0];
+    element.appendChild(link);
+    
     var cont = document.getElementsByTagName("main")[0];
     cont.setAttribute("id", "contenuto");
 }
