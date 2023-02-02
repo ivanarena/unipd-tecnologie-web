96091ec5bd3fb7aa11bd3ee3134ba537c0050978
Fbombi <filippo.bomben@studenti.unipd.it>
2023-02-02 17:54:48 +0100

    ulimo commit ragaaaaaaa

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM php:7.4-apache
-RUN docker-php-ext-install mysqli pdo pdo_mysql
--- php/utils/database.php
+++ php/utils/database.php
@@ -4,14 +4,10 @@ if (strtok($_SERVER["REQUEST_URI"], '?') == '/php/utils/database.php') {
 } else { 
 	class database
 	{
-		private static $dbName = 'sanjunipero';
-		private static $dbHost = 'db';
-		private static $dbUsername = 'root';
-		private static $dbUserPassword = 'password';
-		/*private static $dbName = 'gdentone';
+		private static $dbName = 'fbomben';
 		private static $dbHost = '127.0.0.1:3306';
-		private static $dbUsername = 'gdentone';
-		private static $dbUserPassword = 'xach9Ahnohx7aePu';*/
+		private static $dbUsername = 'fbomben';
+		private static $dbUserPassword = 'FaoZae4aiwiseili';
 		private static $cont = null;
 		private function __construct()
 		{
